185856cb3edae6cacd9afa3c4b5583db1ef02cde
gautamgarima16 <goutamgarima693@gmail.com>
2026-08-24 22:57:16 +0530

    Add files via upload

---
+++ note.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+int amount,count =0;
+printf("enter amount:");
+scanf("%d",&amount);
+count+=amount/500;
+amount%=500;
+count+=amount/200;
+amount%=200;
+count+=amount/100;
+amount%=100;
+count+=amount/50;
+amount%=50;
+count+=amount/20;
+amount%=20;
+count+=amount/10;
+printf("total number of notes=%d",count);
+return 0;
+
+}
